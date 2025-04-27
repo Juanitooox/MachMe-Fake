cfb1e9d643cceb4c38e7d8534214320f7c385c4c
Juan Manuel <chaparrojuan963@gmail.com>
2025-04-27 18:20:05 -0500

    Create splash.js

---
+++ splash.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    // Esperamos 3 segundos antes de mostrar el contenido principal
+    setTimeout(function() {
+        document.getElementById('splash-screen').style.display = 'none';
+        document.getElementById('app-content').style.display = 'block';
+    }, 3000);
+};
